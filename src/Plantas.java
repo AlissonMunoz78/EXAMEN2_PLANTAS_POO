a92c6fcd9e09efd06cc3909eca9439f3733a87a3
Lizzie <alisson.munoz@epn.edu.ec>
2024-11-28 07:22:18 -0500

    commit palantas

--- src/Plantas.java
+++ src/Plantas.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class Plantas {
+    private String color;
+    private String tipoClima;
+    private double tamanio;
+
+    public Plantas(String color,String tipoClima, double tamanio){
+        this.color = color;
+        this.tipoClima = tipoClima;
+        this.tamanio = tamanio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipoClima() {
+        return tipoClima;
+    }
+
+    public void setTipoClima(String tipoClima) {
+        this.tipoClima = tipoClima;
+    }
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+
+
+    public void fotoSintesis() {
+        System.out.println("La planta sintetiza sus propios nutrientes.");
+    }
+
+    public void mostrarDatos() {
+        System.out.println("El color es: " + color);
+        System.out.println("El tipo de clima es: " + tipoClima);
+        System.out.println("El tamaño es: " + tamanio + " metros");
+    }
+
 }
--- src/Vegetales.java
+++ src/Vegetales.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Vegetales {
+    private String nutrientes;
+    private String fibra;
+    private String tipoSabor;
+
+    public Vegetales(String nutrientes, String fibra, String tipoSabor) {
+        this.nutrientes = nutrientes;
+        this.fibra = fibra;
+        this.tipoSabor = tipoSabor;
+    }
+
+    public String getNutrientes() {
+        return nutrientes;
+    }
+
+    public void setNutrientes(String nutrientes) {
+        this.nutrientes = nutrientes;
+    }
+
+    public String getFibra() {
+        return fibra;
+    }
+
+    public void setFibra(String fibra) {
+        this.fibra = fibra;
+    }
+
+    public String getTipoSabor() {
+        return tipoSabor;
+    }
+
+    public void setTipoSabor(String tipoSabor) {
+        this.tipoSabor = tipoSabor;
+    }
 }
